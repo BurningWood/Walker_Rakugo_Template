a01849d60e1d65961e3ff46bbf89f46afd811c32
Zon Petilla <Burningwoodmedia@gmail.com>
2019-07-09 01:09:42 +0900

    Update
    Update

--- game/assets/levels/Test_Level.gd
+++ game/assets/levels/Test_Level.gd
@@ -6,11 +6,13 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if Rakugo.current_root_node != self: 
-		Rakugo.current_root_node = self
-	Rakugo.begin("Hello", name, "hello_world") 
-	Rakugo.add_dialog(self, "hello_world")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	pass
+#	
+# if Rakugo.current_root_node != self: 
+#		Rakugo.current_root_node = self
+#	Rakugo.begin("Hello", name, "hello_world") 
+#	Rakugo.add_dialog(self, "hello_world")
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+##func _process(delta):
+##	pass
